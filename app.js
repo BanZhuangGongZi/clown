5432e4f3750ea489faf2d9e5047cc19fdd0d4928
ke.huang@zmlearn.com <ke.huang@zmlearn.com>
2016-11-08 13:57:11 +0800

    代码格式

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var path = require("path");
+
 var db = require("./models/db.js");
 var router = require('./routers/router.js');
+
 var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
