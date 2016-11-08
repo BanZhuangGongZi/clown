f651bea1dfad0cba18bba96e8dfced476d94af16
ke.huang@zmlearn.com <ke.huang@zmlearn.com>
2016-11-08 12:06:51 +0800

    init project

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var path = require("path");
+var db = require("./models/db.js");
+var router = require('./routers/router.js');
+var app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set(express.static("/public"));
+app.use(function (req, res) {
+    res.render("err");
+});
+app.listen(8083);
